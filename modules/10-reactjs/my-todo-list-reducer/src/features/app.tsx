cfd2bb56cc81a48f5999b701f1c9ca4060a7177b
Nestor Amesty <nestor09amesty@gmail.com>
2020-04-20 21:04:36 +0200

    Update fork and useReducer

---
+++ modules/10-reactjs/my-todo-list-reducer/src/features/app.tsx
@@ -0,0 +1,72 @@
+import React, { useState, useReducer } from 'react'
+import { Todo } from './todo'
+import styles from './app.module.css'
+import { bind } from '../utils/bind'
+
+const cx = bind(styles)
+
+type Action = 'create' | 'complete'
+
+export const App: React.FC = () => {
+
+  const reducer = (state: Todo[], payload: { action: Action, text?: string, id?: number }): Todo[] => {
+    switch(payload.action) {
+      case 'create': {
+        const newTodo: Todo = { id: Math.floor(Math.random() * 1000), text: todoText, completed: false }
+        return [...state, newTodo]
+      }
+
+      case 'complete': {
+        state.map(todo => {
+          if(todo.id === payload.id) {
+            return {
+              ...todo,
+              completed: !todo.completed
+            }
+          }
+          return todo
+        })
+      }
+
+      default: {
+        throw new Error(`Accion '${payload.action}' no soportada`)
+      }
+    }
+  }
+
+  const [todos, dispatch] = useReducer(reducer, [])
+
+
+  const [todoText, setTodoText] = useState('')
+  const isTodoDuplicated = todos.map(todo => todo.text).includes(todoText)
+
+  const clearTodo = () => setTodoText('')
+
+  return (
+    <main>
+      <ul>
+        {todos.map(todo => (
+          <li onClick={() => dispatch({action: 'complete', id: todo.id})} className={cx({ completed: todo.completed })}>
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+      <form
+        onSubmit={event => {
+          event.preventDefault()
+          dispatch({action:'create', text: todoText})
+          clearTodo()
+        }}
+      >
+        <label>
+          Todo
+          <input value={todoText} onChange={event => setTodoText(event.target.value)} />
+        </label>
+        <button onClick={clearTodo}>Clear todo</button>
+        <button type="submit" disabled={isTodoDuplicated}>
+          Create todo
+        </button>
+      </form>
+    </main>
+  )
+}
